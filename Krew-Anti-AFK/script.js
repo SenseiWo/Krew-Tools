b14d11c5112d1bc26f29d3958ef3e3851df375f6
Nitrovesper <79943519+SenseiWo@users.noreply.github.com>
2021-12-16 21:09:52 +0100

    Anti-AFK added by menu

--- Krew-Anti-AFK/script.js
+++
@@ -1,5 +0,0 @@
-setInterval(function unLock() { 
-document.addEventListener ('keydown', function (event){ 
-console.log (event.which); }); 
-var evt = new KeyboardEvent('keydown', {'keyCode':32, 'which':32}); 
-document.dispatchEvent (evt); }, 300000); console.log("Anti-AFK Activated");
