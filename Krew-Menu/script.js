ce6eea509184659b1aa9a429f88293f8efc9cdda
Nitrovesper <79943519+SenseiWo@users.noreply.github.com>
2021-12-16 02:07:36 +0100

    Update script.js

--- Krew-Menu/script.js
+++ Krew-Menu/script.js
@@ -1,178 +1,97 @@
+function sleep(milliseconds) { const date = Date.now();
+let currentDate = null; do { currentDate = Date.now(); } 
+while (currentDate - date < milliseconds); }
 let div = document.createElement('div'); div.id = 'Anti-AFK';
 div.innerHTML = '<center><button style="width: 250px; height: 40px; background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);" onclick="AntiAFK()">Anti-AFK</button></center>';
 document.getElementById("game-settings-modal").appendChild(div);
-document.getElementById("game-settings-modal").appendChild(div);
-let div1 = document.createElement('div'); div1.id = 'AutoFish';
+/* let div1 = document.createElement('div'); div1.id = 'AutoFish';
 div1.innerHTML = '<center><button style="width: 250px; height: 40px; background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);" onclick="AutoFish()">Auto-Fish</button></center>';
-document.getElementById("game-settings-modal").appendChild(div1);
-let div2 = document.createElement('div'); div2.id = 'destroy';
-div2.innerHTML = '<center><button style="width: 250px; height: 40px; background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);" onclick="OOF()">O.O.F</button></center>';
-document.getElementById("game-settings-modal").appendChild(div2);
-let div25 = document.createElement('div'); div25.id = 'PerformanceBoostIDH9522';
-div25.innerHTML = '<center><button style="width: 250px; height: 40px; background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);" onclick="PerformanceBoost()">Performance-Boost (Beta)</button></center>';
-document.getElementById("game-settings-modal").appendChild(div25);
-let div25101 = document.createElement('div'); div25101.id = 'ModernGUI1589';
-div25101.innerHTML = '<center><button style="width: 250px; height: 40px; background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);" onclick="ModernGUI()">Modern GUI</button></center>';
+document.getElementById("game-settings-modal").appendChild(div1); */
+let div25101 = document.createElement('div'); div25101.id = 'ModernGUI15891';
+div25101.innerHTML = '<center><button style="width: 250px; height: 40px; background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);" onclick="ModernGUI()">Modern-GUI</button></center>';
 document.getElementById("game-settings-modal").appendChild(div25101);
-let div2510190 = document.createElement('div'); div2510190.id = 'ModernGUI1589';
-div2510190.innerHTML = '<center><button style="width: 250px; height: 40px; background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);" onclick="CreateClan()">Clan Raid</button></center>';
+let div2510190 = document.createElement('div'); div2510190.id = 'ClanRaidbtn';
+div2510190.innerHTML = '<center><button style="width: 250px; height: 40px; background-image: linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red);" onclick="CreateClan()">Clan-Bomb</button></center>';
 document.getElementById("game-settings-modal").appendChild(div2510190);
 let div000 = document.createElement('div'); div.id = 'hiddenID9245';
 div000.innerHTML = '<br>';
 document.getElementById("game-settings-modal").appendChild(div000);
-let div251010 = document.createElement('div'); div251010.id = 'ModernGUI11589';
-div251010.innerHTML = '<p>To usse Clan Raid you need an account</p>';
-document.getElementById("game-settings-modal").appendChild(div251010);
 document.getElementById("game-settings-modal").style.border = "thick solid black";
 document.getElementById("toggle-game-settings-modal-button").style.border = "2px solid gold";
-function OOF() {
-var Enabled = new Audio('https://www.mboxdrive.com/1639430771346-voicemaker.in-speech.mp3'); 
-Enabled.play();
-document.getElementById("exit-island-button").click();
-var el1 = document.getElementById('game-settings-modal');
-var el2 = document.getElementById('modal-dialog');
-var el3 = document.getElementById('minimap-container');
-var el4 = document.getElementById('leaderboard');
-var el5 = document.getElementById('top-menu-ui');
-var el6 = document.getElementById('chat-local');
-var el7 = document.getElementById('experience-ui');
-var el8 = document.getElementById('krew-hud');
-var el8 = document.getElementById('splash-modal');
-el1.remove(); el2.remove(); el3.remove(); el8.remove();
-el4.remove(); el5.remove(); el6.remove(); el7.remove(); 
-document.addEventListener ('keydown', function (event){  console.log (event.which); }); 
-var evt = new KeyboardEvent('keydown', {'keyCode':32, 'which':32}); 
-document.dispatchEvent (evt); 
-document.addEventListener ('keydown', function (event){  console.log (event.which); }); 
-var evt = new KeyboardEvent('keydown', {'keyCode':87, 'which':87}); 
-document.dispatchEvent (evt); 
-let div80 = document.createElement('div');
-div80.id = 'txt564524';
-div80.innerHTML = '<p>OOF mode enabled</p>';
-div80.innerHTML = '<h4>OOF mode enabled</h4><a href="https://krew.io/"><button>Click here to reload krew.io</button</a>';
-document.getElementById("chat-div").appendChild(div80);
-document.title = "OOF mode enabled";
-let div20 = document.createElement('div'); div20.id = 'Remove9458';
-div20.innerHTML = '<button onclick="Remove2()">RemoveMe</button>';
-document.getElementById("chat-div").appendChild(div20); console.log("OOF"); }
-console.log("Script loaded! Contact: Nitrovesper#8395 for help.");
 function AntiAFK() {
+document.getElementById("close-game-settings-modal-button").click();
+document.getElementById("close-help-button").click();
+document.getElementById("close-quests-button").click();
 var Enabled = new Audio('https://www.mboxdrive.com/1639430555164-voicemaker.in-speech.mp3'); 
 Enabled.play();
 document.addEventListener ('keydown', function (event){ 
 console.log (event.which); }); 
 var evt = new KeyboardEvent('keydown', {'keyCode':32, 'which':32}); 
-document.dispatchEvent (evt);
-let div801 = document.createElement('div');
-div801.id = 'txt019542';
-div801.innerHTML = '<h4>Anti-AFK mode enabled</h4>';
-document.getElementById("chat-div").appendChild(div801);
-let div20 = document.createElement('div'); div20.id = 'Remove20201';
-div20.innerHTML = '<button onclick="Remove1()">RemoveMe</button>';
-document.getElementById("chat-div").appendChild(div20);
-document.title = "Anti-AFK mode enabled";
-console.log("Anti-AFK Activated"); }
+document.dispatchEvent (evt); }
 function Remove1() {
 var el1A = document.getElementById('Remove20201');
 var el2A = document.getElementById('txt019542');
 el1A.remove(); el2A.remove(); }
-function Remove2() {
-var el1B = document.getElementById('Remove9458');
-var el2B = document.getElementById('txt564524');
-el1B.remove(); el2B.remove(); }
-function Remove3() {
-var el1C = document.getElementById('Remove0259');
-var el2C = document.getElementById('txt456410');
-el1C.remove(); el2C.remove(); }
 function Remove4() {
-var el1D = document.getElementById('txt4564110');
-var el2D = document.getElementById('Remove1157');
+var el1D = document.getElementById('textID7952');
+var el2D = document.getElementById('txt91252');
 el1D.remove(); el2D.remove(); }
-
 function Remove5() {
 var el1C = document.getElementById('txt45641110');
 var el2C = document.getElementById('R1emove1157');
 el1C.remove(); el2C.remove(); }
-
 function AutoFish() {
+document.getElementById("close-game-settings-modal-button").click();
+document.getElementById("close-help-button").click();
+document.getElementById("close-quests-button").click();
 var Enabled = new Audio('https://www.mboxdrive.com/1639430555164-voicemaker.in-speech.mp3'); 
 Enabled.play();
 document.addEventListener ('keydown', function (event){ 
 console.log (event.which); }); 
 var evt = new KeyboardEvent('keydown', {'keyCode':50, 'which':50}); 
 document.dispatchEvent (evt); 
-document.title = "Auto-Fish mode enabled";
-alert("It is currently not possible to fish automatically! This will come in the next update.");
-console.log("FunctionPlayerGet-FishingRod"); }
-function sleep(milliseconds) {
-const date = Date.now();
-let currentDate = null; do {
-currentDate = Date.now(); } 
-while (currentDate - date < milliseconds); }
-function PerformanceBoost() { sleep(500);
-var Enabled = new Audio('https://www.mboxdrive.com/1639430555164-voicemaker.in-speech.mp3'); 
-Enabled.play();
-document.getElementById("quality-list").value = "1";
-function Start() { Field.start(); }
-var Field = { field: document.createElement("canvas"),
-start: function () { this.field.height = 800;
-this.field.width = 800;
-this.context = this.field.getContext("2d");
-document.body.insertBefore(this.field, document.body.childNodes[0]);
-this.interval = setInterval(fieldUpdate, 10);
-window.addEventListener('keydown', function (e) {
-Field.keys = Field.keys || [];
-Field.keys[e.keyCode] = (e.type == "keydown"); })
-window.addEventListener('keyup', function (e) {
-Field.keys[e.keyCode] = (e.type == "keydown"); }) },
-clear: function () { this.context.clearRect(0, 0, this.field.width, this.field.height); } }
-function component(color, width, height, x, y) {
-this.color = color; this.width = width;
-this.height = height; this.x = x; this.y = y;
-this.componentUpdate = function () {
-ctx = Field.context; ctx.save(); ctx.fillStyle = color;
-ctx.fillRect(this.x, this.y, this.width, this.height); } }
-function fieldUpdate() { Field.clear();
-myAvatar.componentUpdate();
-CharacterData.componentUpdate();
-if (Field.keys && Field.keys[37]) { myAvatar.x--; var origCoord = [myAvatar.x, myAvatar.y]; }
-if (Field.keys && Field.keys[38]) { myAvatar.y--; var origCoord = [myAvatar.x, myAvatar.y]; }
-if (Field.keys && Field.keys[39]) { myAvatar.x++; var origCoord = [myAvatar.x, myAvatar.y]; }
-if (Field.keys && Field.keys[40]) { myAvatar.y++; var origCoord = [myAvatar.x, myAvatar.y]; } }
-let div85111 = document.createElement('div');
-div85111.id = 'txt4564110';
-div85111.innerHTML = '<h4>PerformanceBoost mode enabled</h4>';
-document.getElementById("chat-div").appendChild(div85111);
-let div2109 = document.createElement('div'); div2109.id = 'Remove1157';
-div2109.innerHTML = '<button onclick="Remove4()">RemoveMe</button>';
-document.getElementById("chat-div").appendChild(div2109);
-document.title = "Performance mode enabled";
-console.log("Performance Boost Activated!"); }
+document.title = "Auto-Fish mode enabled"; }
 function ModernGUI() {
+document.getElementById("chat-history").style.color = "blue";
+document.getElementById("close-game-settings-modal-button").click();
+document.getElementById("close-help-button").click();
+document.getElementById("close-quests-button").click();
 var Enabled = new Audio('https://www.mboxdrive.com/1639430555164-voicemaker.in-speech.mp3'); 
 Enabled.play();
-document.title = "Modern-GUI mode enabled";
-document.getElementById("leaderboard").style.border = "2px solid gold";
-document.getElementById("leaderboard").style.outline = "3px solid black";
+document.title = "Krew.io - Modern-GUI";
+document.getElementById("leaderboard").style.border = "2px solid black";
+document.getElementById("leaderboard").style.borderRadius = "3px";
 document.getElementById("leaderboard").style.color = "black";
 document.getElementById("leaderboard").style.backgroundImage = "url('https://i.ibb.co/JBS1gg4/img.jpg')";
-document.getElementById("minimap-container").style.backgroundImage = "url('https://i.ibb.co/bLBGXXD/pirate-treasure-map.jpg')";
-document.getElementById("close-game-settings-modal-button").click();
-document.getElementById("toggle-game-settings-modal-button").click();
-document.getElementById("game-settings-modal").style.backgroundImage = "url('https://i.ibb.co/JBS1gg4/img.jpg')";
-document.getElementById("krew-hud").style.backgroundImage = "url('https://i.ibb.co/c6Xwpm4/img8.jpg')";
+document.getElementById("krew-hud").style.backgroundImage = "url('https://i.ibb.co/K2mL8RH/978285.gif')";
+document.getElementById("krew-hud").style.borderRadius = "3px";
+document.getElementById("krew-hud").style.border = "2px solid black";
+document.getElementById("krew-hud").style.height = "140px";
+document.getElementById("toggle-invite-link-button").style.border = "1px solid black";
+document.getElementById("exit-island-button").style.border = "1px solid black";
 document.getElementById("show-map-text").style.color = "black";
-let div851111 = document.createElement('div');
-div851111.id = 'txt45641110';
-div851111.innerHTML = '<h4>Moder-GUI enabled</h4>';
-document.getElementById("chat-div").appendChild(div851111);
-let div21091 = document.createElement('div'); div21091.id = 'R1emove1157';
-div21091.innerHTML = '<button onclick="Remove5()">RemoveMe</button>';
-document.getElementById("chat-div").appendChild(div21091);
-setInterval(function(){ document.getElementById("game-settings-modal").style.border = "6px solid gold"; }, 500); 
-setInterval(function(){ document.getElementById("game-settings-modal").style.border = "6px solid black"; }, 1000);  }
+document.getElementById("game-settings-modal").style.backgroundImage = "url('https://i.ibb.co/JBS1gg4/img.jpg')";
+document.getElementById("minimap-container").style.backgroundImage = "url('https://i.ibb.co/bLBGXXD/pirate-treasure-map.jpg')";
+document.getElementById("experience-attribute-container").style.border = "2px solid white";
+document.getElementById("experience-attribute-container").style.borderRadius = "10px";
+document.getElementById("player-leaderboard").style.border = "2px solid blue";
+document.getElementById("player-leaderboard").style.borderRadius = "2px";
+document.getElementById("player-leaderboard").style.color = "white";
+document.getElementById("player-leaderboard").style.backgroundColor = "black";
+document.getElementById("game-settings-modal").style.border = "2px solid red";
+document.getElementById("chat-div").style.border = "2px solid silver";
+document.getElementById("top-menu-ui").style.border = "2px solid blue";
+document.getElementById("fps-wrapper").style.border = "2px solid black";
+document.getElementById("fps-wrapper").style.backgroundColor = "black";
+document.getElementById("fps-wrapper").style.color = "white";
+document.getElementById("ping-wrapper").style.border = "2px solid black";
+document.getElementById("ping-wrapper").style.backgroundColor = "black";
+document.getElementById("ping-wrapper").style.color = "white";
+document.getElementById("experience-ui").style.border = "1px solid black"; }
 function CreateClan() {
 document.getElementById("close-game-settings-modal-button").click();
+document.getElementById("close-help-button").click();
+document.getElementById("close-quests-button").click();
 setInterval(function CreateClan90() {
 var CreateRequest= document.getElementById("clan-request");
 var l = 4, s = "";
